e972dd33b503cf55d1694ba7cdee02622db09a8d
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-09-30 18:44:40 +0100

    testing making moves

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+from cell import Cell
 from board import Board
+
+grid = [[Cell() for i in range(8)] for j in range(8)]
+grid[0][2].token = "x"
+grid[0][2].is_king = True
+grid[1][1].token = "o"
+grid[1][1].is_king = False
 
 b = Board()
 print(b)
-start, end = b.convert_input_to_coords("3a-4k")
-print(b.turn)
-print(start, end)
-print(b.is_valid_move(start, end))
+start, end = b.convert_input_to_coords("3g 4h")
+b = b.make_move(start, end)
+start, end = b.convert_input_to_coords("4h 5g")
+b = b.make_move(start, end)
+start, end = b.convert_input_to_coords("3e d4")
+b = b.make_move(start, end)
+start, end = b.convert_input_to_coords("c3 d4")
+b = b.make_move(start, end)
